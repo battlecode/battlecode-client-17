e934e1eaa5a3962b223311988f563893dab8b4d3
Gina Yuan <gina.yuan@gmail.com>
2016-12-21 13:19:54 -0500

    added art from Howard

--- src/imageloader.ts
+++ src/imageloader.ts
@@ -6,22 +6,22 @@ export type AllImages = {
   background: Image,
   unknown: Image,
   tree: {
-    fullHealth: Image, 
-    lowHealth: Image, 
-    sapling: Image, 
+    fullHealth: Image,
+    lowHealth: Image,
+    sapling: Image,
   },
   bullet: {
-    fast: Image, 
-    medium: Image, 
-    slow: Image, 
+    fast: Image,
+    medium: Image,
+    slow: Image,
   },
   robot: {
-    archon: Array<Image>, 
-    gardener: Array<Image>, 
-    lumberjack: Array<Image>, 
-    recruit: Array<Image>, 
-    scout: Array<Image>, 
-    soldier: Array<Image>, 
+    archon: Array<Image>,
+    gardener: Array<Image>,
+    lumberjack: Array<Image>,
+    recruit: Array<Image>,
+    scout: Array<Image>,
+    soldier: Array<Image>,
     tank: Array<Image>,
     bulletTree: Array<Image>
   },
@@ -63,37 +63,37 @@ export function loadAll(config: Config, finished: (AllImages) => void) {
   img(result.tree, 'lowHealth', require('./img/map/low_health_tree.png'));
   img(result.tree, 'sapling', require('./img/map/sapling.png'));
 
-  img(result.bullet, 'fast', require('./img/bullets/fast_bullet.png'));
-  img(result.bullet, 'medium', require('./img/bullets/medium_bullet.png'));
-  img(result.bullet, 'slow', require('./img/bullets/slow_bullet.png'));
+  img(result.bullet, 'fast', require('./img/bullets/bullet_fast.png'));
+  img(result.bullet, 'medium', require('./img/bullets/bullet_medium.png'));
+  img(result.bullet, 'slow', require('./img/bullets/bullet_slow.png'));
 
   // these are the teams we expect robots to be in according to current
   // battlecode-server
-  // TODO(jhgilles): 
+  // TODO(jhgilles):
   // we'll need to update them if team configuration becomes more dynamic
-  img(result.robot.archon, 0, require('./img/sprites/archon_white.png'));
-  img(result.robot.archon, 1, require('./img/sprites/archon_black.png'));
+  img(result.robot.archon, 0, require('./img/sprites/archon_red.png'));
+  img(result.robot.archon, 1, require('./img/sprites/archon_blue.png'));
   img(result.robot.archon, 2, require('./img/sprites/archon_neutral.png'));
-  img(result.robot.bulletTree, 0, require('./img/sprites/bullet_tree_black.png'));
-  img(result.robot.bulletTree, 1, require('./img/sprites/bullet_tree_white.png'));
+  img(result.robot.bulletTree, 0, require('./img/sprites/bullet_tree_blue.png'));
+  img(result.robot.bulletTree, 1, require('./img/sprites/bullet_tree_red.png'));
   img(result.robot.bulletTree, 2, require('./img/sprites/bullet_tree_neutral.png'));
-  img(result.robot.gardener, 0, require('./img/sprites/gardener_white.png'));
-  img(result.robot.gardener, 1, require('./img/sprites/gardener_black.png'));
+  img(result.robot.gardener, 0, require('./img/sprites/gardener_red.png'));
+  img(result.robot.gardener, 1, require('./img/sprites/gardener_blue.png'));
   img(result.robot.gardener, 2, require('./img/sprites/gardener_neutral.png'));
-  img(result.robot.lumberjack, 0, require('./img/sprites/lumberjack_white.png'));
-  img(result.robot.lumberjack, 1, require('./img/sprites/lumberjack_black.png'));
+  img(result.robot.lumberjack, 0, require('./img/sprites/lumberjack_red.png'));
+  img(result.robot.lumberjack, 1, require('./img/sprites/lumberjack_blue.png'));
   img(result.robot.lumberjack, 2, require('./img/sprites/lumberjack_neutral.png'));
-  img(result.robot.recruit, 0, require('./img/sprites/recruit_white.png'));
-  img(result.robot.recruit, 1, require('./img/sprites/recruit_black.png'));
+  img(result.robot.recruit, 0, require('./img/sprites/recruit_red.png'));
+  img(result.robot.recruit, 1, require('./img/sprites/recruit_blue.png'));
   img(result.robot.recruit, 2, require('./img/sprites/recruit_neutral.png'));
-  img(result.robot.scout, 0, require('./img/sprites/scout_white.png'));
-  img(result.robot.scout, 1, require('./img/sprites/scout_black.png'));
+  img(result.robot.scout, 0, require('./img/sprites/scout_red.png'));
+  img(result.robot.scout, 1, require('./img/sprites/scout_blue.png'));
   img(result.robot.scout, 2, require('./img/sprites/scout_neutral.png'));
-  img(result.robot.soldier, 0, require('./img/sprites/soldier_white.png'));
-  img(result.robot.soldier, 1, require('./img/sprites/soldier_black.png'));
+  img(result.robot.soldier, 0, require('./img/sprites/soldier_red.png'));
+  img(result.robot.soldier, 1, require('./img/sprites/soldier_blue.png'));
   img(result.robot.soldier, 2, require('./img/sprites/soldier_neutral.png'));
-  img(result.robot.tank, 0, require('./img/sprites/tank_white.png'));
-  img(result.robot.tank, 1, require('./img/sprites/tank_black.png'));
+  img(result.robot.tank, 0, require('./img/sprites/tank_red.png'));
+  img(result.robot.tank, 1, require('./img/sprites/tank_blue.png'));
   img(result.robot.tank, 2, require('./img/sprites/tank_neutral.png'));
 }
 
