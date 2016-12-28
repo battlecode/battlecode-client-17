9e80b77050bb96faac4e74f6ca1d48edbf5d54a6
James Gilles <jhgilles@mit.edu>
2016-12-27 21:59:14 -0500

    Initial electron setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-// This file is magic.
+// This is all james' fault
+// Don't worry, he eventually repented and became an electrical engineer
 
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+var conf = {
   context: path.resolve(__dirname, 'src'),
   entry: {
     app: './app.ts',
@@ -15,16 +17,67 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.png', '.jpg', '.glsl']
+    // add `.ts` as a resolvable extension.
+    extensions: ['.ts', '.js', '.png', '.jpg']
   },
-  devtool: 'source-map',
   module: {
-    loaders: [
-      { test: /\.ts$/, loader: 'ts-loader' },
-      { test: /\.(png|jpg)$/, loader: 'url?limit=10000' },
-      { test: /\.glsl$/, loader: 'raw' }
+    rules: [
+      { test: /\.ts$/, loader: 'awesome-typescript-loader' },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=10000&name=[name]-[hash:base64:7].[ext]' }
     ]
-  },
-  plugins: []
+  }
+};
+
+module.exports = function(env) {
+  env = env || {};
+
+  if (env.dev) {
+    // we're in dev
+    conf = merge(conf, {
+      devtool: 'source-map',
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.LoaderOptionsPlugin({
+          minimize: false,
+          debug: true
+        }),
+      ]
+    });
+  } else {
+    // we're compiling for prod
+    conf = merge(conf, {
+      plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        }),
+      ]
+    });
+  }
+
+  if (env.electron) {
+    // we're compiling for electron
+    conf = merge(conf, {
+      target: 'electron-renderer',
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.ELECTRON': true
+        })
+      ],
+      // electron will find './bc17/thing.ext' but won't find '/bc17/thing.ext'
+      output: { publicPath: './bc17/' }
+    });
+  } else {
+    // we're compiling for the browser
+    conf = merge(conf, {
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.ELECTRON': false
+        })
+      ]
+    });
+  }
+
+  return conf;
 };
