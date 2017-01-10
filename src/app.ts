24a181157d036ee5dc586f287cbc8c23138cb01d
Aaron Vontell <vontell@mit.edu>
2017-01-10 16:02:09 -0500

    Matches updated from Run Match, fixes #88, fixes #97

--- src/app.ts
+++ src/app.ts
@@ -118,6 +118,7 @@ export default class Client {
     }
     this.clearScreen();
     this.currentGame = game;
+    this.matchqueue.refreshGameList(this.games, this.currentGame ? this.currentGame: 0, this.currentMatch ? this.currentMatch: 0);
   }
 
   setMatch(match: number) {
@@ -227,7 +228,12 @@ export default class Client {
           if (this.games.length === 1) {
             this.setGame(0);
             this.setMatch(0);
+            this.matchqueue.refreshGameList(this.games, this.currentGame ? this.currentGame: 0, this.currentMatch ? this.currentMatch: 0);
           }
+        },
+        // What to do with any other match
+        () => {
+          this.matchqueue.refreshGameList(this.games, this.currentGame ? this.currentGame: 0, this.currentMatch ? this.currentMatch: 0);
         }
       );
     }
