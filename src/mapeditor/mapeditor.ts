b6b9162f67fc1912480dc79e33fe30c47a5c4ce1
Aaron Vontell <vontell@mit.edu>
2017-01-10 22:07:52 -0500

    Typing within inputs should not trigger shortcuts in map editor, fixes #83

--- src/mapeditor/mapeditor.ts
+++ src/mapeditor/mapeditor.ts
@@ -83,17 +83,22 @@ export default class MapEditor {
    */
   onkeydown(): (event: KeyboardEvent) => void {
     return (event: KeyboardEvent) => {
-      switch (event.keyCode) {
-        case 67: // "c" - Toggle Circle Bots
-        this.conf.circleBots = !this.conf.circleBots;
-        this.form.render();
-        break;
-        case 83: // "s" - Set (Add/Update)c
-        this.form.addToMap();
-        break;
-        case 68: // "d" - Delete
-        this.form.deleteFromMap();
-        break;
+      
+      console.log(document.activeElement.nodeName)
+      var input = document.activeElement.nodeName == "INPUT";
+      if(!input) {
+        switch (event.keyCode) {
+          case 67: // "c" - Toggle Circle Bots
+          this.conf.circleBots = !this.conf.circleBots;
+          this.form.render();
+          break;
+          case 83: // "s" - Set (Add/Update)c
+          this.form.addToMap();
+          break;
+          case 68: // "d" - Delete
+          this.form.deleteFromMap();
+          break;
+        }
       }
     };
   }
